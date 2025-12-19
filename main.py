dc9e3ac5717f87dce7465ae95759f935e6041e83
liuyuwei <B11317027@gemail.yuntech.edu.tw>
2025-12-19 15:51:58 +0800

    set cookie

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends, HTTPException, status, Response, Cookie
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from typing import Optional
 from pydantic import BaseModel
 import sqlite3
 
@@ -23,6 +27,38 @@ CREATE TABLE IF NOT EXISTS users(
 """)
 first_conn.commit()
 first_conn.close()
+
+# 登入後的認證token
+SECRET_KEY = "secret-key-ohya"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIER_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7 
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data: dict, expires_delta : Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp" : expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+    return encode_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms = ALGORITHM)
+        account = payload.get("sub")
+        if account is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        return account
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp" : expire, "type" : "refresh"})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+    return encode_jwt
 
 app = FastAPI()
 
@@ -51,19 +87,77 @@ def register_userdata(req: LoginRequest):
         return {"success": False, "message": f"註冊發生錯誤: {e}"}
 
 @app.post("/login")
-def login_userdata(req: LoginRequest):
+def login_userdata(req: LoginRequest, response: Response = None):
     with sqlite3.connect("user.db") as conn:
         cur = conn.cursor()
 
-        cur.execute("SELECT password FROM users WHERE account = ?", (req.account,))
+        cur.execute("SELECT account, password FROM users WHERE account = ?", (req.account,))
         row = cur.fetchone()
 
         if row is None:
             return {"success": False, "message": "使用者不存在，請先註冊"}
-        if row[0] == req.password:
-            return {"success": True, "message": "登入成功"}
+        if row[1] != req.password:
+            return {"success": False, "message": "密碼錯誤"}
         
-        return {"success": False, "message": "密碼錯誤"}
+        access_token = create_access_token({"sub" : row[0]})
+        refresh_token = create_refresh_token({"sub" : row[0]})
+
+        response.set_cookie(
+            key = "jwt",
+            value = access_token,
+            httponly = True,
+            samesite = "lax"
+        )
+
+        response.set_cookie(
+            key = "refresh",
+            value = refresh_token,
+            httponly = True,
+            samesite = "lax",
+            max_age = 7 * 24 * 60 *60
+        )
+
+        
+        return {
+            "success": True, 
+            "message": "登入成功",
+            "access_token": access_token,
+            "token_type": "bearer"
+        }
+
+@app.post("/refresh")
+def refresh_token(refresh: Optional[str] = Cookie(None)):
+    if refresh is None:
+        raise HTTPException(status_code=401, detail="Missing refresh token")
+    
+    try:
+        payload = jwt.decode(refresh, SECRET_KEY, algorithms = ALGORITHM)
+        if payload.get("type") != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+        account = payload.get("sub")
+        if account is None:
+            raise HTTPException(status_code=401, detail="Invalid refresh token")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
+    
+    new_access_token = create_access_token({"sub": account})
+    new_refresh_token = create_refresh_token({"sub", account})
+
+    response = Response()
+    response.set_cookie(
+        key = "jwt",
+        value = new_access_token,
+        httponly = True,
+        samesite = "lax"
+    )
+
+    response.set_cookie(
+        key = "refresh",
+        value = new_refresh_token,
+        httponly = True,
+        samesite = "lax",
+        max_age = 7 * 24 * 60 *60
+    )
 
 @app.get("/home", response_class=HTMLResponse)
 def home_page():
